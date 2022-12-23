d865128bebc20b8768e8594239d74f50bd3f621e
Crystal <c19931114@yahoo.com.tw>
2022-12-23 23:10:55 +0800

    pod install SnapKit

--- FugoAssignment/LaunchViewController.swift
+++ FugoAssignment/LaunchViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class LaunchViewController: BaseViewController {
     
+    lazy var logoImageView: UIImageView = {
+        let image = UIImage(named: "Fugo_logo")
+        let imageView = UIImageView(image: image)
+        imageView.backgroundColor = .clear
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    private func setupUI() {
+        view.addSubview(logoImageView)
+        logoImageView.snp.makeConstraints {
+            $0.width.height.equalTo(view.snp.width).dividedBy(2)
+           $0.center.equalTo(view)
+        }
     }
 }
---
+++ FugoAssignment/UIView+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIView+Extension.swift
+//  FugoAssignment
+//
+//  Created by Crystal on 2022/12/23.
+//
+
+import UIKit
+import SnapKit
+
+extension UIView {
+    
+}
