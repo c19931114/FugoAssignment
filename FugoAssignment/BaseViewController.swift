8813233544b82b0178724f93d910c15f14016134
Crystal <c19931114@yahoo.com.tw>
2022-12-23 22:25:56 +0800

    first commit

---
+++ FugoAssignment/BaseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseViewController.swift
+//  FugoAssignment
+//
+//  Created by Crystal on 2022/12/23.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+}
+
---
+++ FugoAssignment/LaunchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LaunchViewController.swift
+//  FugoAssignment
+//
+//  Created by Crystal on 2022/12/23.
+//
+
+import UIKit
+
+class LaunchViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
